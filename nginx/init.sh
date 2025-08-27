2e1ad39abbfbd2e4af6422603b8c4f9c1b401ab6
Fabrizio Miranda <fabrizio.miranda@efestodev.com>
2025-08-27 15:42:23 +0200

    fix ssl

--- nginx/init.sh
+++ nginx/init.sh
@@ -1,40 +1,6 @@
 #!/bin/sh
-set -eux
+set -eu
 
-echo "--- Inizio script di inizializzazione Nginx ---"
-
-# Assicura che la variabile SERVER_NAME sia presente
-: "${SERVER_NAME:?SERVER_NAME is required}"
-
-echo "SERVER_NAME impostato a: ${SERVER_NAME}"
-
-# Crea la configurazione di Nginx dal template
 envsubst '$SERVER_NAME' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
 
-echo "File di configurazione Nginx creato in /etc/nginx/conf.d/default.conf"
-cat /etc/nginx/conf.d/default.conf
-
-# Definisce i percorsi per i certificati
-LIVE_DIR="/etc/letsencrypt/live/${SERVER_NAME}"
-FULL="${LIVE_DIR}/fullchain.pem"
-PRIV="${LIVE_DIR}/privkey.pem"
-
-# Controlla se il certificato esiste
-if [ ! -f "$FULL" ]; then
-  echo ">>> Certificato non trovato in ${FULL}. Generazione di un certificato fittizio..."
-  
-  # Crea la directory, se non esiste
-  mkdir -p "$LIVE_DIR"
-  
-  # Esegui il comando openssl e MOSTRA L'OUTPUT in caso di errori
-  openssl req -x509 -nodes -newkey rsa:2048 -days 1 \
-    -keyout "$PRIV" -out "$FULL" -subj "/CN=${SERVER_NAME}"
-    
-  echo ">>> Comando openssl completato."
-  echo ">>> Verifico i file creati nel volume:"
-  ls -lR /etc/letsencrypt
-else
-  echo ">>> Certificato trovato. Salto la generazione."
-fi
-
-echo "--- Fine script. Avvio di Nginx... ---"
+echo "Configurazione di Nginx creata per ${SERVER_NAME}"
